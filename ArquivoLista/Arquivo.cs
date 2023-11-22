3067a63c0e00e5406b6fbc0f459c7526284bd799
ricar <ricar@FROHLICHNB>
2023-11-22 20:03:56 -0300

    Adicionar arquivos de projeto.

---
+++ ArquivoLista/Arquivo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArquivoLista
+{
+    internal class Arquivo
+    {
+        private string _nome;
+        private StreamWriter _sw;
+        private StreamReader _sr;
+        public string Mensagem { get; set; }
+        
+        public string Nome {
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    _nome = value;
+                }                
+            } 
+        }
+        public Arquivo(string nome)
+        {
+            Nome = nome;
+        }
+
+        public void CriaAbreArquivo()
+        {
+            _sw = new StreamWriter("C:\\Arquivo\\" + Nome + ".txt", true, Encoding.UTF8);
+        }
+
+        public void LerArquivo()
+        {
+            string linha;
+            _sr = new StreamReader("C:\\Arquivo\\" + Nome + ".txt");
+            linha = _sr.ReadLine();
+            while(linha!= null)
+            {
+                Console.WriteLine(linha);
+                linha = _sr.ReadLine();
+            }
+            _sr.Close();
+        }
+
+        public void GravarMensagem(string mensagem)
+        {
+            _sw.WriteLine(mensagem);
+        }
+
+        public void FecharArquivo()
+        {
+            _sw.Close();
+        }
+
+    }
+}
---
+++ ArquivoLista/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ArquivoLista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, mensagem;
+            Console.WriteLine("Digite o nome do arquivo: ");
+            nome = Console.ReadLine();
+            Arquivo a = new Arquivo(nome);
+            int op;
+            while(true)
+            {
+                Console.WriteLine("Menu do sistema de anotações: ");
+                Console.WriteLine("1 - para escrever uma anotação");
+                Console.WriteLine("2 - para ler as anotações");
+                op = int.Parse(Console.ReadLine()); 
+                if(op == 1)
+                {
+                    a.CriaAbreArquivo();
+                    Console.WriteLine("Digite uma mensagem para ser armazenada: ");
+                    Console.WriteLine("Digite SAIR para finalizar!");
+                    while(true)
+                    {
+                        mensagem = Console.ReadLine();
+                        if (mensagem.Equals("SAIR"))
+                        {
+                            a.FecharArquivo();
+                            break;
+                        }
+                        else
+                        {
+                            a.GravarMensagem(mensagem);
+                        }
+                    }
+                }
+                else if(op == 2)
+                {
+                    a.LerArquivo();
+                }
+                else
+                {
+                    Console.WriteLine("Operação inválida!");
+                }
+            }
+        }
+    }
+}
